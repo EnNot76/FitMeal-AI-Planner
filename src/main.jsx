f89d80136ad06c78d1cb7a25778e2169a11a0f70
EnNot76 <enzo.notari@live.de>
2026-06-22 12:14:22 +0200

    Initialize main.jsx with React setup

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+// src/main.jsx
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+// se hai un CSS globale, importa qui:
+// import './styles.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
